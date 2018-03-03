76c534a637fa0e5ed35e45f808f02deb35acba2a
nkprod <nurlybek.karshyga@gmail.com>
2018-03-02 23:04:12 -0500

    Creating new Java class for loginactivity.

--- app/src/main/java/com/example/cars/LoginActivity.java
+++ app/src/main/java/com/example/cars/LoginActivity.java
@@ -1,31 +1,17 @@
 package com.example.cars;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-/**
- * Created by nulrybekkarshyga on 02.03.18.
- */
-
-public class LoginActivity extends AppCompatActivity{
-
-
-
-    //widgets
-    private TextView mRegister;
-    private EditText mEmail, mPassword;
-    private Button mLogin;
-    private ProgressBar mProgressBar;
-    private ImageView mLogo;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mRegister = (TextView) findViewById(R.id.link_register);
@@ -34,7 +20,15 @@ public class LoginActivity extends AppCompatActivity{
         mLogin = (Button) findViewById(R.id.btn_login);
         mLogo = (ImageView) findViewById(R.id.logo);
     }
+    //widgets
+    private TextView mRegister;
+    private EditText mEmail, mPassword;
+    private Button mLogin;
+    private ProgressBar mProgressBar;
+    private ImageView mLogo;
 
 
-}
+
+
+
 }
