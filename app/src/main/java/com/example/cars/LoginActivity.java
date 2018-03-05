57fa262193787c5175508598d6aa2206c4ae7ea6
nkprod <nurlybek.karshyga@gmail.com>
2018-03-05 18:57:09 -0500

    New firebase json

--- app/src/main/java/com/example/cars/LoginActivity.java
+++ app/src/main/java/com/example/cars/LoginActivity.java
@@ -1,14 +1,30 @@
 package com.example.cars;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Firebase
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //widgets
+    private TextView mRegister;
+    private EditText mEmail, mPassword;
+    private Button mLogin;
+    private ProgressBar mProgressBar;
+    private ImageView mLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +34,12 @@ public class LoginActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.input_email);
         mPassword = (EditText) findViewById(R.id.input_password);
         mLogin = (Button) findViewById(R.id.btn_login);
+
         mLogo = (ImageView) findViewById(R.id.logo);
+
     }
-    //widgets
-    private TextView mRegister;
-    private EditText mEmail, mPassword;
-    private Button mLogin;
-    private ProgressBar mProgressBar;
-    private ImageView mLogo;
-
-
-
-
 
 }
+
+
+
