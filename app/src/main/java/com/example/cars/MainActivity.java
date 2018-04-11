51f8aeb93ad73d8d052f3bc615f881f2b7de70b6
aalot001 <aalot001@plattsburgh.edu>
2018-04-10 22:18:02 -0400

    DeletedAllListViewFunctionality-NewFragmentRecyclerViewAndCardView

--- app/src/main/java/com/example/cars/MainActivity.java
+++ app/src/main/java/com/example/cars/MainActivity.java
@@ -1,196 +1,79 @@
 package com.example.cars;
 
 // Ahmed Alotaibi
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.example.cars.account.AccountActivity;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.github.florent37.hollyviewpager.HollyViewPager;
+import com.github.florent37.hollyviewpager.HollyViewPagerConfigurator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 import io.fabric.sdk.android.Fabric;
+import butterknife.ButterKnife;
+import butterknife.Bind;
 
-import static com.example.cars.R.id.swiperefresh;
-
-public class MainActivity extends AppCompatActivity {
-    // Manipulate the the list view for user
-    // which defines in xml
-    private SwipeRefreshLayout swipeRefresh;
-    private ListView carMakesList;
-    private static List <CarMake> carMakes = new ArrayList <>();
-    @SuppressLint("UseSparseArrays")
-    private static Map <Integer, List <Car>> cars = new HashMap <>();
-    private ImageButton settings;
-    private List <CarData> carDataList = new ArrayList <>();
-    // To read the data from the database,
-    DatabaseReference databaseReference =
-            FirebaseDatabase.getInstance().getReference();
-    DatabaseReference carsReference =
-            databaseReference.child("cars");
-    @Override
-    // Call, which functions once the system first creates the activity
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity
+        extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    int pageCount = 5;
+    @Bind(R.id.hollyViewPager)
+    HollyViewPager hollyViewPager;
+    protected void
+    onCreate(Bundle savedInstanceState) {
         // call the super class onCreate
         // to complete the creation of activity like
         // the view hierarchy
         super.onCreate(savedInstanceState);
+        // Set up Crashlytics to get report about any crashes
+        // using Fabric API
         Fabric.with(this, new Crashlytics());
-        getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).hide();
+        }
         setContentView(R.layout.activity_edit_profile);
         setContentView(R.layout.activity_main);
-        carMakesList = findViewById(R.id.carMakesList);
-        carMakesList.setAdapter(new
-                CarMakesListAdapter(this,
-                carMakes));
-        /*swipeRefresh = findViewById(swiperefresh);
-        swipeRefresh.setOnRefreshListener(
-                new SwipeRefreshLayout.OnRefreshListener() {
-                    @Override
-                    public void onRefresh() {
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                // need to refresh context.!
-                                swipeRefresh.setRefreshing(false);
-                            }
-                        }, 5);
-                    }
-                });*/
+        ButterKnife.bind(this);
         BottomNavigationView bottomNavigationView =
                 findViewById(R.id.navigation);
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
         bottomNavigationView.setOnNavigationItemSelectedListener
-                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                (new BottomNavigationView.
+                        OnNavigationItemSelectedListener() {
                     @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    public boolean
+                    onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
-                            case R.id.action_search:
-                                break;
+                            case R.id.action_search:break;
                             case R.id.action_navigation:
                                 Intent intent0 = new
                                         Intent(MainActivity.this,
                                         MapsActivity.class);
-                                startActivity(intent0);
-                                break;
+                                startActivity(intent0);break;
                             case R.id.action_account:
                                 Intent intent1 = new
-                                        Intent(MainActivity.this,
-                                        AccountActivity.class);
-                                startActivity(intent1);
-                                break;
-                        }
-                        return false;
-                    }
-                });
-        settings = (ImageButton) findViewById(R.id.ed_profile);
+                                        Intent(MainActivity.this,AccountActivity.class);
+                                startActivity(intent1);break;}return false;}});
+        ImageButton settings =
+                findViewById(R.id.ed_profile);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, EditProfileActivity.class));
-            }
-        });
-    }
-    // Listen for option item selections
-    // so that we receive a notification
-    // when the user requests a refresh by
-    // selecting the refresh action bar item.
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.swiperefresh:
-                swipeRefresh.setRefreshing(true);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //  This listener to detect the change in the data at a
-        //  particular path (including the children at that particular path).
-        carsReference.addValueEventListener(
-                new ValueEventListener() {
-                    @Override
-                    // “onDataChange” will return the data as an object of DataSnapshot
-                    // and it can be used in a similar manner as depicted in the code.
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        carDataList = new ArrayList <>();
-                        for (DataSnapshot postSnapshot :
-                                dataSnapshot.getChildren()) {
-                            CarData carData = postSnapshot.
-                                    getValue(CarData.class);
-                            carDataList.add(carData);
-                        }
-                        // a list to connect carMakelist to the
-                        // adapter to allow data flew between UIs screen
-                        createCartList();
-                        carMakesList.setAdapter(new
-                                CarMakesListAdapter(
-                                MainActivity.this,
-                                carMakes));
-                    }
-                    @Override
-                    // To handle the error while reading data.
-                    public void onCancelled(DatabaseError databaseError) {
-                        Toast.makeText(MainActivity.this,
-                                "Database is not responding",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
-    // allowing users to pick a car by make id.
-    public static List <Car> getCarsByMakeId(int makeId)
-    {return cars.get(makeId);}
-    @SuppressLint("UseSparseArrays")
-    private void createCartList() {
-        // Store data in an array manner
-        carMakes = new ArrayList <>();
-        cars = new HashMap <>();
-        for (CarData carData : carDataList) {
-            int makeId = carData.getMakeId();
-            CarMake carMake = new
-                    CarMake(makeId,
-                    carData.getMake());
-            if (! carMakes.contains(carMake)) {
-                carMakes.add(carMake);
-            }
-            List <Car> carList = new
-                    ArrayList <>();
-            if (cars.get(makeId) != null) {
-                carList = cars.get(makeId);
-            }
-            //added carId so late we can use
-            // it for the car's picture maybe..!!
-            carList.add(new Car
-                    (carData.getPrice(),
-                            carData.getCarId(),
-                            carData.getYear(),
-                            carData.getModel(),
-                            carData.getDescription()));
-            cars.put(makeId, carList);
-        }
-    }
-}
+            public void
+            onClick(View v) {
+                startActivity(
+                        new Intent(MainActivity.this,
+                        EditProfileActivity.class));}});}}
--- app/src/main/java/com/example/cars/MapsActivity.java
+++ app/src/main/java/com/example/cars/MapsActivity.java
@@ -32,34 +32,26 @@ public class MapsActivity extends AppCompatActivity
         OnMapReadyCallback {
     private static final LatLng Acura = new
             LatLng(42.764911, -73.756344);
-    private static final LatLng AmGeneral = new
-            LatLng(41.682999, -86.244667);
-    private static final LatLng Ironhorse = new
-            LatLng(32.747050, -95.566483);
-
     private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and
+        // get notified when the map is ready to be used.
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         //allow Up navigation with the app icon in the action bar
         getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);}
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Marker
-        // Later on, this marker should
-        // be used for car makers locations
         mMap.animateCamera(
                 CameraUpdateFactory.zoomIn());
         Marker marker = mMap.addMarker(new MarkerOptions()
@@ -67,63 +59,31 @@ public class MapsActivity extends AppCompatActivity
                 .title("Acura Dealer")
                 .icon(BitmapDescriptorFactory.
                         defaultMarker(BitmapDescriptorFactory.
-                                HUE_AZURE))
-                .alpha(0.7f)
-                .draggable(true));
-        marker.setTag(0);
-        mMap.animateCamera(
-                CameraUpdateFactory.zoomIn());
-        marker = mMap.addMarker(new MarkerOptions()
-                .position(AmGeneral)
-                .title("Am General Dealer")
-                .icon(BitmapDescriptorFactory.
-                        defaultMarker(BitmapDescriptorFactory.
-                                HUE_AZURE))
-                .alpha(0.7f)
-                .draggable(true));
-        marker.setTag(0);
-        mMap.animateCamera(
-                CameraUpdateFactory.zoomIn());
-        marker = mMap.addMarker(new MarkerOptions()
-                .position(Ironhorse)
-                .title("Ironhorse Dealer")
-                .icon(BitmapDescriptorFactory.
-                        defaultMarker(BitmapDescriptorFactory.
-                                HUE_AZURE))
-                .alpha(0.7f)
-                .draggable(true));
-        marker.setTag(0);
+                                HUE_AZURE)).alpha(0.7f)
+                .draggable(true));marker.setTag(0);
         mMap.setOnMarkerClickListener(this);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.map_options, menu);
-        return true;
-    }
+        return true;}
     // Allow user to choice different types of Maps
     public boolean onOptionsItemSelected(MenuItem item) {
         // Change the map type based on the user's selection.
         switch (item.getItemId()) {
             case R.id.normal_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                return true;
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);return true;
             case R.id.hybrid_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                return true;
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);return true;
             case R.id.satellite_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                return true;
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);return true;
             case R.id.terrain_map:
-                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                return true;
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);return true;
             case R.id.home:
-                onBackPressed();
-                return true;
+                onBackPressed();return true;
             default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+                return super.onOptionsItemSelected(item);}}
    @Override
     public boolean onMarkerClick(Marker marker) {
         //Integer clickCount = (Integer) marker.getTag();
@@ -131,22 +91,5 @@ public class MapsActivity extends AppCompatActivity
         ": click on the icon in the " +
                 "down-right below to get the direction",
                 Toast.LENGTH_SHORT).show();
-       try {
-           Thread.sleep(10);
-       } catch (InterruptedException e) {
-           e.printStackTrace();
-       }
-       return false;
-    }
-    // Get back to the  parent activity
-    @Override
-    public void onBackPressed(){
-        super.onBackPressed();
-        this.finish();
-    }
-    @Override
-    public boolean onSupportNavigateUp(){
-        onBackPressed();
-        return true;
-    }
-}
+       try {Thread.sleep(10);}catch(InterruptedException e) {
+           e.printStackTrace();}return false;}}
