f2d563c48344d870067417fc5c6f513353e0685a
aalot001 <aalot001@plattsburgh.edu>
2018-04-11 00:36:31 -0400

    MethodsForAsyncTask-MoreCardViewsForImages-etc

--- app/src/main/java/com/example/cars/ScrollViewFragment.java
+++ app/src/main/java/com/example/cars/ScrollViewFragment.java
@@ -1,15 +1,36 @@
 package com.example.cars;
-// Modified By Ahmed Alotaibi
+// Ahmed Alotaibi
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.github.florent37.hollyviewpager.HollyViewPagerBus;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -31,6 +52,18 @@ public class ScrollViewFragment
     ObservableScrollView scrollView;
     @Bind(R.id.title)
     TextView title;
+    @Bind(R.id.intImageView)
+    ImageView intImageView;
+    @Bind(R.id.extImageView)
+    ImageView extImageView;
+    @Bind(R.id.upholsteryImageView)
+    ImageView upholsteryImageView;
+    @Bind(R.id.trimImageView)
+    ImageView trimImageView;
+    @Bind(R.id.rimImageView)
+    ImageView rimImageView;
+    @Bind(R.id.engineImageView)
+    ImageView engineImageView;
     public static ScrollViewFragment
     newInstance(String title){
         Bundle args = new Bundle();
@@ -43,17 +76,69 @@ public class ScrollViewFragment
     @Override
     public View
     onCreateView(@NonNull LayoutInflater inflater,
-                 ViewGroup container, Bundle savedInstanceState) {
+                 ViewGroup container,
+                 Bundle savedInstanceState) {
+        // loadImages(VIN);
         return inflater.inflate(R.layout.fragment_scroll,
                 container, false);}
     /*Called after onCreateView has returned, but before any
       saved state has been restored in to the view.*/
     @Override
-    public void onViewCreated(@NonNull View view,
-                              Bundle savedInstanceState) {
+    public void
+    onViewCreated(@NonNull View view,
+                  Bundle savedInstanceState) {
         super.onViewCreated(view,
                 savedInstanceState);
         ButterKnife.bind(this, view);
         assert getArguments() != null;
-        title.setText(getArguments().getString("title"));
-        HollyViewPagerBus.registerScrollView(getActivity(), scrollView);}}
+        String titleString =
+                getArguments().getString("title");
+        String vin;
+        title.setText(titleString);
+        assert titleString != null;
+        switch (titleString) {
+            case "Merc" +
+                    "edes 0" :
+                vin = "/WDD2130" +
+                        "331A123456/";break;
+            default:
+                vin = "/WDD2130" +
+                        "331A123456/";break;}
+        loadImages(vin,
+                "vehicle");
+        loadImages(vin,
+                "components");
+        //loadImages(vin,"components/equipments");
+        HollyViewPagerBus.registerScrollView(getActivity(), scrollView);}
+    private void
+    loadImageFromUrl(String url,ImageView imageView)
+    {Picasso.with(getContext())
+            .load(url).placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess(){}
+                    @Override
+                    public void onError() {}});}
+    @SuppressLint("StaticFieldLeak")
+    private void
+    loadImages(final String vin,
+               final String specificUrl) {
+        new AsyncTask<URL,
+                Void,
+                List<String>>() {
+            @Override
+            protected List<String> doInBackground(URL... urls) {
+                // Create URL object
+                URL urlVehicle = null;return null;}
+            @Override
+            protected void
+            onPostExecute(List<String> vehicleImages) {}
+        }.execute();}
+    private static
+    List<String> extractImagesFromJson(
+            String jsonResponse,
+            String specificUrl) { return null;}
+    private static String
+    readFromStream(InputStream inputStream)
+            throws IOException {return null;}}
