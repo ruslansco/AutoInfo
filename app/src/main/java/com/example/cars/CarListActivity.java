6981df2706b5cf409945d3fd29ad619487de8d6d
nkprod <nurlybek.karshyga@gmail.com>
2018-05-09 13:45:00 -0400

    Added navigation to the second listview

--- app/src/main/java/com/example/cars/CarListActivity.java
+++ app/src/main/java/com/example/cars/CarListActivity.java
@@ -3,10 +3,18 @@ package com.example.cars;
 
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
+
+import com.example.cars.account.AccountActivity;
 
 import java.util.Objects;
 
@@ -26,5 +34,46 @@ public class CarListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int makeId = intent.getIntExtra("makeId", 1);
         carList.setAdapter(new CarListAdapter(this, MainActivity.getCarsByMakeId(makeId), makeId));
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation1);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.
+                        OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean
+                    onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_search:
+                                Intent intent0 = new
+                                        Intent(CarListActivity.this,
+                                        MainActivity.class);
+                                startActivity(intent0);
+                                break;
+                            case R.id.action_navigation:
+                                Intent intent1 = new
+                                        Intent(CarListActivity.this,
+                                        MapsActivity.class);
+                                startActivity(intent1);
+                                break;
+                            case R.id.action_chat:
+                                Intent intent2 = new
+                                        Intent(CarListActivity.this,
+                                        ChatActivity.class);
+                                startActivity(intent2);
+                                break;
+                            case R.id.action_account:
+                                Intent intent3 = new Intent(
+                                        CarListActivity.this, AccountActivity.class);
+                                startActivity(intent3);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
     }
 }
